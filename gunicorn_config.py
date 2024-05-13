7763b98fe2c8a042873b64e0f448d861c6c17898
cloud_user <cloud_user@example.com>
2024-05-13 11:46:23 -0400

    dev4

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,3 +6,4 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
